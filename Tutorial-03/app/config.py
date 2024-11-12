710f53fd32d7bff6dc147b2a4a5af9a060a5597a
syukur.musa <syukur.musa@cloud-connect.asia>
2024-11-12 09:13:52 +0800

    update

--- Tutorial-03/app/config.py
+++ Tutorial-03/app/config.py
@@ -2,9 +2,9 @@ import os
 
 # DB
 user = os.getenv('POSTGRES_USER','admin')
-password = os.getenv('POSTGRES_PASSWORD','s3cr3t')
-host = os.getenv('POSTGRES_HOST','178.128.118.126')
-database = os.getenv('POSTGRES_DB','syukur_db')
+password = os.getenv('POSTGRES_PASSWORD','qwe123***')
+host = os.getenv('POSTGRES_HOST','localhost')
+database = os.getenv('POSTGRES_DB','fastapi')
 port = os.getenv('POSTGRES_PORT','5432')
 
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
